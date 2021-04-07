d0d8274a0b3c2de8361e6782445caaa613d28727
krissemicolon <krissemicolon@gmail.com>
2021-04-07 19:31:22 +0200

    added basic tui setup

--- README.md
+++ README.md
@@ -16,3 +16,22 @@ cabal build
 ### Dependencies
 - [brick](https://hackage.haskell.org/package/brick)
 - [gitlib](https://hackage.haskell.org/package/gitlib)
+- [directory](https://hackage.haskell.org/package/directory)
+- [cursor](https://hackage.haskell.org/package/cursor)
+- [vty](https://hackage.haskell.org/package/vty)
+
+### Brick architecture
+```
+         Start
+           |
+           V
+Event -> State -> Draw
+  ^                 |
+  |                 |
+   \-    Brick   <-/
+```
+```
+s: State                : TuiState
+e: Custom events
+n: Name of resources    : ResourceName
+```
--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Brick
-
-ui :: Widget ()
-ui = str "perplex"
+import System.Directory
+import Brick.Main
+import Brick.AttrMap
+import Brick.Types
+import Brick.Widgets.Core
+import Graphics.Vty.Input.Events
 
 main :: IO ()
-main = simpleMain ui
+main = do
+    initialState <- buildInitialState
+    endState <- defaultMain tuiApp initialState
+    print endState
+
+data TuiState =
+    TuiState
+    deriving (Show, Eq)
+
+data ResourceName =
+    ResourceName
+    deriving (Show, Eq, Ord)
+
+tuiApp :: App TuiState e ResourceName
+tuiApp =
+    App {
+        appDraw = drawTui,
+        appChooseCursor = showFirstCursor,
+        appHandleEvent = handleTuiEvent,
+        appStartEvent = pure,
+        appAttrMap = const $ attrMap mempty []
+    }
+
+buildInitialState :: IO TuiState
+buildInitialState = pure TuiState
+
+drawTui :: TuiState -> [Widget ResourceName]
+drawTui _ts = []
+
+handleTuiEvent :: TuiState -> BrickEvent n e -> EventM n (Next TuiState)
+handleTuiEvent s e =
+    case e of
+    VtyEvent vtye ->
+        case vtye of
+        EvKey (KChar 'q') [] -> halt s
+        _ -> continue s
+    _ -> continue s
