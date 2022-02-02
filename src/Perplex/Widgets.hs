1d1a808cd92d3595c6237118e7c97c78361ab3a8
krissemicolon <kris@krishuber.xyz>
2022-02-02 13:03:40 +0100

    sync

--- src/Perplex/Widgets.hs
+++ src/Perplex/Widgets.hs
@@ -11,8 +11,14 @@ import Brick.Widgets.Border.Style (unicode)
 frame :: Widget () -> Widget ()
 frame w = withBorderStyle unicode $ borderWithLabel (str "perplex") $ w
 
-homeLayout :: Widget () -> Widget () -> Widget ()
+homeLayout :: Widget () => w -> w -> w
 homeLayout ws wh = ws <+> vBorder <+> wh
+
+commitLayout :: Widget () w => w -> w -> w
+commitLayout wm wh = wm <+> vBorder <+> wh
+
+cmessage :: String -> Widget ()
+cmessage s = str s
 
 status :: Widget ()
 status = str "status"
